8a5baf2c587c803c04b19c57f3057216f8674f6e
Samuel Burns <sam@Samuels-MacBook-Air.local>
2024-10-20 14:17:45 -0700

    added matrix struct plot

--- conditionnum.py
+++ conditionnum.py
@@ -1,6 +1,7 @@
 # samuel burns - hw4 - october 19 2024
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 def makeK(n):
@@ -30,24 +31,44 @@ def condNum(kmat, subarr):
     return cn
 
 
-def plotCn(n):
+def plotCnandMatStruct(n):
     kmat = makeK(n)
     cnmatrix = np.zeros(n - 1)
+    # passes largest kmat, uses i to create a subarray
     for i in range(2, n + 1):
-        cnmatrix[i - 2] = condNum(
-            kmat, i
-        )  # passes largest kmat, uses i to create a subarray
-        print("Progress: ", i)
+        cnmatrix[i - 2] = condNum(kmat, i)
+        # passes largest kmat, uses i to create a subarray
+
+        # did this for fun!
+        sys.stdout.write(f"\rSolving Eigenvalues: {int(i/n * 100)}%")
+        sys.stdout.flush()
     nvals = np.arange(2, n + 1)
-    print(len(nvals))
-    print(len(cnmatrix))
+
+    # conditions number plot
+    plt.figure(num="figure 1")
     plt.style.use("Solarize_Light2")
     plt.plot(nvals, cnmatrix, c="grey", linestyle="-", label="Condition Number")
-    plt.title("Condition Numbers of K-Matrix versus n")
+    plt.title("Condition Numbers of $K_n$ versus n")
     plt.xlabel(f"n (from 2 to {n})")  # sprintf equivalent
     plt.ylabel("Condition Number")
     plt.legend(loc="upper center")
+
+    # matrix struct plots
+    fig, (sp1, sp2, sp3) = plt.subplots(1, 3)
+    fig.suptitle("Matrix Structure Visualization", fontsize="20")
+
+    sp1.matshow(makeK(10))
+    sp1.set_title("$K_{10}$")
+    sp1.set_xlabel("cols")
+    sp1.set_ylabel("rows")
+
+    sp2.matshow(makeK(50))
+    sp2.set_title("$K_{50}$")
+
+    sp3.matshow(makeK(100))
+    sp3.set_title("$K_{100}$")
+
     plt.show()
 
 
-plotCn(1000)
+plotCnandMatStruct(1000)
